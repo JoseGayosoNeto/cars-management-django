44bfa85b68e30b2567896f27b9feb450047c4481
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-02-26 12:01:26 -0300

    feat: add some data validations

--- cars/forms.py
+++ cars/forms.py
@@ -6,4 +6,14 @@ class NewCarModelForm(forms.ModelForm):
         model = Car
         fields = '__all__'
         
+    def clean_value(self): 
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Minimum car value is R$ 20000.')
+        return value
     
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Minimum car factory year is 1975')
+        return factory_year
