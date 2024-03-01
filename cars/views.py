6f5e360354e9b9c882d079839cff1341268d48fd
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-03-01 09:33:30 -0300

    refactor: change cars app views from FBVs to CBVs

--- cars/views.py
+++ cars/views.py
@@ -1,23 +1,28 @@
 from django.shortcuts import render, redirect
+from django.views import View
 from cars.models import Car
 from cars.forms import NewCarModelForm
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
+class CarsView(View):
     
-    query_params = request.GET.get('search')
-    if query_params:
-        # cars = Car.objects.filter(model__icontains=query_params)
-        cars = cars.filter(model__icontains=query_params)
-    
-    return render(request, 'cars.html', {'cars': cars})
+    def get(self, request):
+        cars = Car.objects.all().order_by('model')
+        
+        query_params = request.GET.get('search')
+        if query_params:
+            cars = cars.filter(model__icontains=query_params)
+            
+        return render(request, 'cars.html', {'cars': cars})
 
-def new_car_view(request):
-    if request.method == "POST":
+class NewCarView(View):
+    
+    def get(self, request):
+        new_car_form = NewCarModelForm()
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
+    
+    def post(self, request):
         new_car_form = NewCarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = NewCarModelForm()
-    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+        return render(request, 'new_car.html', {'new_car_form': new_car_form})
